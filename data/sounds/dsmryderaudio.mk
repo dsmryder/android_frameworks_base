fac14cb674dce72a4171fd4570e0baf20155684a
dsmryder <dsmryder@gmail.com>
2012-07-15 18:13:09 -0400

    adding my ringtone make file again
    Change-Id: Id4abb32357d64d7683d43b839a5615ff1269b8f6

--- data/sounds/dsmryderaudio.mk
+++ data/sounds/dsmryderaudio.mk
@@ -1,6 +1,3 @@
-#
-# Ringtone set just for my monkey butt
-# 
 # This file is included from core.mk so that all devices will have these sounds
 # 
 # TODO: Clean up for future releases
@@ -10,7 +7,7 @@ LOCAL_PATH:= frameworks/base/data/sounds
 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/Alarm_Rooster_02.ogg:system/media/audio/alarms/Alarm_Rooster_02.ogg \
-	$(LOCAL_PATH)/Ring_Classic_02.ogg:system/media/audio/alarms/Ring_Classic_02.ogg \
+	$(LOCAL_PATH)/Alarm_Beep_01.ogg:system/media/audio/alarms/Alarm_Beep_01.ogg \
 	$(LOCAL_PATH)/effects/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
 	$(LOCAL_PATH)/effects/KeypressStandard.ogg:system/media/audio/ui/KeypressStandard.ogg \
 	$(LOCAL_PATH)/effects/KeypressSpacebar.ogg:system/media/audio/ui/KeypressSpacebar.ogg \
@@ -18,10 +15,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/effects/KeypressReturn.ogg:system/media/audio/ui/KeypressReturn.ogg \
 	$(LOCAL_PATH)/effects/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
 	$(LOCAL_PATH)/effects/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
-	$(LOCAL_PATH)/effects/LowBattery.ogg:system/media/audio/ui/LowBattery.ogg \
-	$(LOCAL_PATH)/effects/Lock.ogg:system/media/audio/ui/Lock.ogg \
-	$(LOCAL_PATH)/effects/Unlock.ogg:system/media/audio/ui/Unlock.ogg \
-	$(LOCAL_PATH)/notifications/TheNewStyle.ogg:system/media/audio/notifications/TheNewStyle.ogg \
-	$(LOCAL_PATH)/newwavelabs/OnTheHunt.ogg:system/media/audio/notifications/OnTheHunt.ogg \
 	$(LOCAL_PATH)/newwavelabs/Playa.ogg:system/media/audio/ringtones/Playa.ogg \
-	$(LOCAL_PATH)/newwavelabs/ringtones/daddyringingringtone.ogg:system/media/audio/ringtones/daddyringtone.ogg \
+	$(LOCAL_PATH)/ringtones/SweetChildOMine.ogg:system/media/audio/ringtones/SweetChildOMine.ogg \
+	$(LOCAL_PATH)/ringtones/daddyringingringtone.ogg:system/media/audio/ringtones/daddyringingringtone.ogg \
+	$(LOCAL_PATH)/notifications/TheNewStyle.ogg:system/media/audio/notifications/TheNewStyle.ogg
